3e585b06b7b820dbaf02382fe818af6e55cf229e
Carolina Serrao <carolina.serrao@RJBTHLDDEV01BOOK04.local>
2018-05-05 10:47:47 -0300

    Flask-RESTPlus Response Marshaling

--- app.py
+++ app.py
@@ -7,17 +7,20 @@ api = Api(app)
 language_dto = api.model('Language', {'language': fields.String('The language.')})
 
 language = []
-python = {'language' : 'python'}
+python = {'language' : 'python', 'id': 1}
 language.append(python)
 
 @api.route('/language')
 class Languafe(Resource):
+    @api.marshal_with(language_dto, envelope='data')
     def get(self):
         return language
 
     @api.expect(language_dto)
     def post(self):
-        language.append(api.payload)
+        new_language = api.payload
+        new_language['id'] = len(language) + 1
+        language.append(new_language)
         return {'result': 'Language added'}, 201
 
 if __name__ == '__main__':
